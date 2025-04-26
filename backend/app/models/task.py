8d9ac6f2cd31b4dd068ef4c86f4de220026c12db
Aaron <aaron@aaroncody.com>
2025-04-26 08:25:41 -0700

    Added optional URI param to createTask Added Result column to UI

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -2,8 +2,9 @@ from pydantic import BaseModel
 
 class TaskRequest(BaseModel):
     duration: int               # in seconds, to simulate long-running work
+    uri: str | None = None      # [optional] S3 URI pointing to bucket containing task input artifacts
 
 class TaskStatus(BaseModel):
-    task_id: str
-    status: str
-    result: str | None = None
+    task_id: str                # UUID of task
+    status: str                 # States: PENDING->RUNNING->[COMPLETED/FAILED]
+    result: str | None = None   # [optional] string output of task (could also be an S3 URI pointing to artifacts)
