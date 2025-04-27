3254bc1e29d3ad02d82348cf7e6593de73acd246
Aaron <aaron@aaroncody.com>
2025-04-27 05:33:26 -0700

    bumped duration on custom task

--- backend/app/tasks/1f983fea-41d2-43ca-8f59-dffa99d1cba3.py
+++ backend/app/tasks/1f983fea-41d2-43ca-8f59-dffa99d1cba3.py
@@ -2,7 +2,7 @@ import time,random,sys
 
 def task(task_id: str, uri: str):
         sys.stdout.write("input -> "+uri)   # inputs to fake task in this S3 bucket
-        duration = random.randint(1,10)
+        duration = random.randint(10,30)
         time.sleep(duration)          # Simulate long task
         sys.stdout.write(", processed in "+str(duration)+"s, ") 
         sys.stdout.write("output -> "+uri)   # results of fake task in this S3 bucket
